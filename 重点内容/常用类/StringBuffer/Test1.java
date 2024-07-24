b4f94ed6fd7b657a1bd51fe7f1b910aaf8ac6c77
2096164171 <2096164171@qq.com>
2024-07-24 18:24:16 +0000

    第三次提交

--- 重点内容/常用类/StringBuffer/Test1.java
+++ 重点内容/常用类/StringBuffer/Test1.java
@@ -1,10 +1,22 @@
 package 常用类.StringBuffer;
 //在实际的开发中频繁使用字符串的拼接会有什么问题？
+// java中的字符串是不可变的，每次拼接都会产生新的字符串对象，占用大量方法区内存空间，效率低
+// String s＝"Hello”;
+// s+="World";
+// 上面的代码在方法区中产生了三个对象
+// Hello
+// World 
+// HelloWorld
 public class Test1 {
     public static void main(String[] args) {
         System.out.println(".()");
-        System.out.println("()()");
-
+        String s="";
+        // 这样做会给字符串方法区常量池造成很大的压力
+        for (int i = 0; i < 10; i++) {
+            s+=i;
+            System.out.println(s);
+        }
+        
         
     }
 }
---
+++ 重点内容/常用类/StringBuffer/Test2.java
@@ -0,0 +1,32 @@
+package 常用类.StringBuffer;
+
+import java.io.StringBufferInputStream;
+
+// 如果以后要进行大量的字符串拼接,用加号会创建大量字符串对像，建议使用StringBuffer或者是StringBuilder
+// java.lang.StringBuffer;
+// java.lang.StringBuilder;
+public class Test2 {
+    public static void main(String[] args) {
+        // StringBuffer底层是一个byte数组，初始化容量是16
+        StringBuffer stringBuffer=new StringBuffer();
+        // 可以用append方法进行字符串拼接
+        // append方法如果在byte数组16的容量用完会自动扩容
+        stringBuffer.append("a");
+        stringBuffer.append("b");
+        stringBuffer.append("c");  
+        stringBuffer.append(3.14);
+        stringBuffer.append(true);
+        //自动会调用toString方法
+        System.out.println(stringBuffer); 
+        // 如何优化StringBuffer性能
+        // 在创建StringBuffer对象的时候，给定一个初始容量，最好减少底层byte数组扩容的次数
+        // 下面是指定初始化容量的StringBuffer对象(字符串缓冲区对象)
+        StringBuffer sb=new StringBuffer(100);
+        //StringBuilder方法
+        
+           
+         
+
+
+    }
+}
